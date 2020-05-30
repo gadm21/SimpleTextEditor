04ff6f267c4d4cca55b58fa674f6433070aba198
unknown <g.gad@nu.edu.eg>
2020-05-30 17:33:23 +0200

    back suckerrs!!!

--- main.py
+++ main.py
@@ -317,7 +317,7 @@ class Main(QtWidgets.QMainWindow):
 
     def select(self, word_positions) :
         cursor = self.text.textCursor()
-        print("block:", cursor.blockNumber(), " col:", cursor.columnNumber())
+        
         for pos_start, word_len in word_positions : 
             cursor.setPosition(pos_start+ cursor.blockNumber()) 
             cursor.setPosition(pos_start+word_len+cursor.blockNumber(), QtGui.QTextCursor.KeepAnchor) 
@@ -405,113 +405,27 @@ class Main(QtWidgets.QMainWindow):
                 event.ignore()
 
     def contextMenuEvent(self, event):
-        contextMenu = QtGui.QMenu(self)
+        contextMenu = QtWidgets.QMenu(self)
         newAct = contextMenu.addAction("New")
         openAct = contextMenu.addAction("Open")
         quitAct = contextMenu.addAction("Quit")
+        well = contextMenu.addAction("well")
         action = contextMenu.exec_(self.mapToGlobal(event.pos()))
         if action == quitAct:
             self.close()
-        
+    
+    def no(self):
+        print("no") 
+
     def context(self,pos):
+        self._normalMenu = QtWidgets.QMenu(self.text)
+        self._addCustomMenuItems(self._normalMenu)
+        self._normalMenu.exec_(QtGui.QCursor.pos())
 
-        # Grab the cursor
-        cursor = self.text.textCursor()
-
-        # Grab the current table, if there is one
-        table = cursor.currentTable()
-
-        # Above will return 0 if there is no current table, in which case
-        # we call the normal context menu. If there is a table, we create
-        # our own context menu specific to table interaction
-        if table:
-
-            menu = QtGui.QMenu(self)
-
-            appendRowAction = QtWidgets.QAction("Append row",self)
-            appendRowAction.triggered.connect(lambda: table.appendRows(1))
-
-            appendColAction = QtWidgets.QAction("Append column",self)
-            appendColAction.triggered.connect(lambda: table.appendColumns(1))
-
-
-            removeRowAction = QtWidgets.QAction("Remove row",self)
-            removeRowAction.triggered.connect(self.removeRow)
-
-            removeColAction = QtWidgets.QAction("Remove column",self)
-            removeColAction.triggered.connect(self.removeCol)
-
-
-            insertRowAction = QtWidgets.QAction("Insert row",self)
-            insertRowAction.triggered.connect(self.insertRow)
-
-            insertColAction = QtWidgets.QAction("Insert column",self)
-            insertColAction.triggered.connect(self.insertCol)
-
-
-            mergeAction = QtWidgets.QAction("Merge cells",self)
-            mergeAction.triggered.connect(lambda: table.mergeCells(cursor))
-
-            # Only allow merging if there is a selection
-            if not cursor.hasSelection():
-                mergeAction.setEnabled(False)
-
-
-            splitAction = QtWidgets.QAction("Split cells",self)
-
-            cell = table.cellAt(cursor)
-
-            # Only allow splitting if the current cell is larger
-            # than a normal cell
-            if cell.rowSpan() > 1 or cell.columnSpan() > 1:
-
-                splitAction.triggered.connect(lambda: table.splitCell(cell.row(),cell.column(),1,1))
-
-            else:
-                splitAction.setEnabled(False)
-
-
-            menu.addAction(appendRowAction)
-            menu.addAction(appendColAction)
-
-            menu.addSeparator()
-
-            menu.addAction(removeRowAction)
-            menu.addAction(removeColAction)
-
-            menu.addSeparator()
-
-            menu.addAction(insertRowAction)
-            menu.addAction(insertColAction)
-
-            menu.addSeparator()
-
-            menu.addAction(mergeAction)
-            menu.addAction(splitAction)
-
-            # Convert the widget coordinates into global coordinates
-            pos = self.mapToGlobal(pos)
-
-            # Add pixels for the tool and formatbars, which are not included
-            # in mapToGlobal(), but only if the two are currently visible and
-            # not toggled by the user
-
-            if self.toolbar.isVisible():
-                pos.setY(pos.y() + 45)
-
-            if self.formatbar.isVisible():
-                pos.setY(pos.y() + 45)
-                
-            # Move the menu to the new position
-            menu.move(pos)
-
-            menu.show()
-
-        else:
-
-            event = QtGui.QContextMenuEvent(QtGui.QContextMenuEvent.Mouse,QtCore.QPoint())
-
-            self.text.contextMenuEvent(event)
+    def _addCustomMenuItems(self, menu) :
+        menu.addSeparator()
+        menu.addAction(u'Test', self.no)
+        
 
     def removeRow(self):
 
